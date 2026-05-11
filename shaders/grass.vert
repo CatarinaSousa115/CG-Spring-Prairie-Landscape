bc3039c169331d8c8db7661838a191ca2282199c
Joao Silva <up202306429@up.pt>
2026-05-11 18:17:42 +0100

    fixed grass display

--- shaders/grass.vert
+++ shaders/grass.vert
@@ -1,27 +1,21 @@
-attribute vec3 position;
-attribute vec2 texCoord;
+// shaders/grass.vert
+attribute vec3 aVertexPosition;
+attribute vec2 aTextureCoord;
 
-uniform mat4 projectionMatrix;
-uniform mat4 modelViewMatrix;
-
+uniform mat4 uPMatrix;
+uniform mat4 uMVMatrix;
 uniform float time;
 
 varying vec2 vTexCoord;
 
 void main() {
+    vec3 pos = aVertexPosition;
 
-    vec3 pos = position;
+    float wave = sin(time * 2.0 + pos.x * 5.0 + pos.z * 3.0) * 0.08;
+    
+    pos.x += wave * aTextureCoord.y; 
 
-    float wave =
-        sin(time * 2.0 + position.x * 5.0)
-        * 0.08;
+    gl_Position = uPMatrix * uMVMatrix * vec4(pos, 1.0);
 
-    pos.x += wave * texCoord.y;
-
-    gl_Position =
-        projectionMatrix *
-        modelViewMatrix *
-        vec4(pos, 1.0);
-
-    vTexCoord = texCoord;
+    vTexCoord = aTextureCoord;
 }
--- world/Flora/Grass/GrassPatch.js
+++ world/Flora/Grass/GrassPatch.js
@@ -1,40 +1,27 @@
 import { GrassBlade } from "./GrassBlade.js";
-import { CGFappearance } from "../../../../lib/CGF.js";
 
 export class GrassPatch {
   constructor(scene, options = {}) {
     this.scene = scene;
-
     this.blade = new GrassBlade(scene);
-
     this.blades = [];
 
-    const count = options.count ?? 40;
+    const count = 3;
 
     for (let i = 0; i < count; i++) {
       this.blades.push({
-        x: (Math.random() - 0.5) * 2,
-        z: (Math.random() - 0.5) * 2,
-        rotation: Math.random() * Math.PI * 2,
-        scale: 0.6 + Math.random() * 0.8,
+        rotation: (i * Math.PI) / count,
+        scale: 0.8 + Math.random() * 0.4,
       });
     }
-
-    this.appearance = new CGFappearance(scene);
-    this.appearance.setAmbient(0.2, 0.5, 0.1, 1);
-    this.appearance.setDiffuse(0.3, 0.7, 0.2, 1);
   }
 
   display() {
-    this.appearance.apply();
-
     for (const blade of this.blades) {
       this.scene.pushMatrix();
 
-      this.scene.translate(blade.x, 0, blade.z);
       this.scene.rotate(blade.rotation, 0, 1, 0);
-      this.scene.scale(blade.scale, blade.scale, blade.scale);
-
+      this.scene.scale(blade.scale * 3.0, blade.scale, blade.scale);
       this.blade.display();
 
       this.scene.popMatrix();
